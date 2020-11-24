fc523ec9215d68d4a06ea1ed11f58b1c9ce70e4f
lukepark327 <lukepark327@gmail.com>
2020-11-24 20:30:27 +0900

    add sleep @ exp.sh

--- electrum/exp.sh
+++ electrum/exp.sh
@@ -6,9 +6,23 @@ mkdir res
 # loops
 for n in `seq 1 1 100`; do
         echo \#$n
-        sh run.sh
-        sleep 180.
+
+        # sh run.sh
+        ./run_electrum daemon -d
+        sleep 10.
+        ./run_electrum getinfo
+
+        sleep 300.
+
+        # sh stop.sh
+        ./run_electrum getinfo
+        ./run_electrum stop
+        sleep 10.
+
         mv download_verify.txt ./res/$n
-        sh stop.sh
-        sh clean.sh
+
+        # sh clean.sh
+        rm -rf ~/.electrum
+        rm -f download_verify.txt
+        sleep 10.
 done
